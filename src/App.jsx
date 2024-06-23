135fb223c2322d11d6c1ed0e610b68875884b1a1
PhamVietHoangFPT <hoangpvse183123@fpt.edu.vn>
2024-06-23 15:04:42 +0700

    Update default is home

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from './components/pages/Home'
 import News from './components/pages/News'
 import Contact from './components/pages/Contact'
 import About from './components/pages/About'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Display from './components/pages/Display'
 import { useEffect } from 'react'
 import { gapi } from 'gapi-script'
@@ -36,9 +36,8 @@ export default function App() {
           <Route path='/news' element={<News />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
           <Route path='/about' element={<About />}></Route>
-
+          <Route path="/" element={<Navigate replace to="/home" />} />
         </Routes>
-        
         <Footer></Footer>
       </BrowserRouter>
     </GoogleOAuthProvider>
