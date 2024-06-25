17c198c63f44905305926ddc36e4cfa10ab29295
PhamVietHoangFPT <hoangpvse183123@fpt.edu.vn>
2024-06-25 18:17:43 +0700

    Change login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -3,21 +3,21 @@ const clientId = "629470625241-il5it1rn5pejaltppdm23jog45iat57b.apps.googleuserc
 import { jwtDecode } from 'jwt-decode';
 
 function Login() {
-  const responseMessage = (response) => {
+  const response = (response) => {
     const decoded = jwtDecode(response.credential);
     localStorage.setItem("userToken", response.credential);
     localStorage.setItem("userInfo", JSON.stringify(decoded));
     window.location.reload();
   }
-  const errorMessage = (error) => {
+  const error = (error) => {
     console.log(error)
   }
 
   return (
     <GoogleLogin
       clientId={clientId}
-      onSuccess={responseMessage}
-      onError={errorMessage}
+      onSuccess={response}
+      onError={error}
     />
   )
 }
